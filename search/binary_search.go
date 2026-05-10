0466d2cbcf045acd520ae2b5e5d6ae68ba1c63dd
Stepan Protsak <stephan.protsack@gmail.com>
2026-05-10 13:00:11 +0200

    Add binary search

--- search/binary_search.go
+++ search/binary_search.go
@@ -1,5 +1,28 @@
 package search
 
+// BinarySearch search a slice of integers using
+// binary search algorithm.
 func BinarySearch(inp []int, target int) int {
-	return -1
+	start := 0
+	end := len(inp)
+	return binarySearch(inp, target, start, end)
+}
+
+func binarySearch(inp []int, target int, start, end int) int {
+	if start >= end {
+		return -1
+	}
+
+	idx := start + (end-start)/2
+	val := inp[idx]
+
+	if val == target {
+		return idx
+	}
+
+	if target < val {
+		return binarySearch(inp, target, start, idx)
+	}
+
+	return binarySearch(inp, target, idx+1, end)
 }
